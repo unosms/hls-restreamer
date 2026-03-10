cc92060119ac57d8a9d5cac1be09d392e7986207
Walid Alassaad <unosms.net@gmail.com>
2026-03-10 13:04:09 +0200

    fix: force URL root from current request

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->runningInConsole()) {
+            return;
+        }
+
+        $request = request();
+        $root = rtrim((string) $request->root(), '/');
+        if ($root !== '') {
+            URL::forceRootUrl($root);
+        }
     }
 }
